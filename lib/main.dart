c4da71d6a87bdbadfde184969a21d44998f40fbe
Huotsty <122008167+Huotsty@users.noreply.github.com>
2025-03-31 14:13:08 +0700

    mock and main run

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,58 @@
+import 'package:finish_crud_week8/data/mock_repository/iphone_stock_mock_repository.dart';
+import 'package:finish_crud_week8/ui/providers/iphone_stock_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-Future<void> main() async {
+import 'ui/providers/async_value.dart';
 
-  // Initialize the app
-  WidgetsFlutterBinding.ensureInitialized();
-
+void main() {
   // Run the app
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create:
+          (context) =>
+              IphoneStockProvider(repository: IphoneStockMockRepository()),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: StockListScreen()),
+    );
+  }
+}
+
+class StockListScreen extends StatelessWidget {
+  const StockListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Access provider data
+    final provider = context.watch<IphoneStockProvider>();
+    final iphoneStock = provider.iphoneStock;
+    final iphoneStockAsyncValue = provider.iphoneStockAsyncValue;
+    if (iphoneStockAsyncValue.state == AsyncValueState.loading) {
+      return Center(child: CircularProgressIndicator());
+    } else if (iphoneStockAsyncValue.state == AsyncValueState.error) {
+      return Center(child: Text(iphoneStockAsyncValue.error.toString()));
+    } else {
+      return ListView.builder(
+        itemCount: iphoneStock.length,
+        itemBuilder: (context, index) {
+          final stock = iphoneStock[index];
+          return ListTile(
+            title: Text(stock.iphoneModel),
+            subtitle: Text('Price: \$${stock.price}'),
+            trailing: Text('Stock: ${stock.quantity}'),
+          );
+        },
+      );
+    }
+  }
 }
--- lib/ui/providers/iphone_stock_provider.dart
+++ lib/ui/providers/iphone_stock_provider.dart
@@ -4,22 +4,22 @@ import 'package:flutter/material.dart';
 import '../../models/iphone_stock.dart';
 import 'async_value.dart';
 
-class IphoneStockProvider extends ChangeNotifier{
-  List<IphoneStock> _iphoneStock =[];
+class IphoneStockProvider extends ChangeNotifier {
+  List<IphoneStock> _iphoneStock = [];
   late AsyncValue<List<IphoneStock>> iphoneStockAsyncValue;
   final IPhoneStockRepository repository;
 
-  IphoneStockProvider({required this.repository}){
-    iphoneStockAsyncValue = AsyncValue.loading();
+  IphoneStockProvider({required this.repository}) {
     fetchIphoneStock();
   }
-  
+  List<IphoneStock> get iphoneStock => _iphoneStock;
   Future<void> fetchIphoneStock() async {
     iphoneStockAsyncValue = AsyncValue.loading();
     notifyListeners();
     try {
       _iphoneStock = await repository.getIPhoneStock();
-        // 3  Handle success
+      _iphoneStock = _iphoneStock.toList();
+      // 3  Handle success
       iphoneStockAsyncValue = AsyncValue.success(_iphoneStock);
     } catch (e) {
       iphoneStockAsyncValue = AsyncValue.error(e.toString());
@@ -27,5 +27,4 @@ class IphoneStockProvider extends ChangeNotifier{
       notifyListeners();
     }
   }
-  
 }
